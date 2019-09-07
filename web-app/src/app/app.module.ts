eff0c52cd75d02e1ba3438597478286930f11bd3
Hassan Kamran <hassan@oraclemachines.com>
2019-09-07 18:09:11 +0500

    Reset Angular Web App

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CaptionedImageComponent } from './captioned-image/captioned-image.component';
-import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CaptionedImageComponent,
-    TeamComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
